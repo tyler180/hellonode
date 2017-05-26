3a571e1ba8bba86cf84cd377f3b8a45e721c8d80
tyler180 <tyler_180@hotmail.com>
2017-05-26 17:53:24 +0000

    q!updated settings

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ var server = http.createServer(function (request, response) {
 
 // listen on localhost:8000
 server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+console.log("Server listening at http://174.138.56.128:8000/");
